64e9285d3701051c4187fbbb18418a24d87cae21
DeazStar <deadfromtheborn@gmail.com>
2022-12-13 10:23:29 -0700

    bug fix. add if parent does not exsit return NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,6 +22,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
+	if (!parent)
+	{
+		return (NULL);
+	}
+
 	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
